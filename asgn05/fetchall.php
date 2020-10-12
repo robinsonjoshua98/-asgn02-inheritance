279dd12cd6100388aa2576e46a16761ae4aacf3b
Joshua Robinson <joshuarobinson@students.abtech.edu>
2020-10-11 22:39:23 -0400

    commit

--- asgn05/fetchall.php
+++ asgn05/fetchall.php
@@ -4,13 +4,13 @@ include 'connect.php';
 
 $stmt = $db->query("SELECT * FROM names");
 
-$results = $stmt->fetchAll();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 foreach($results as $row) {
-    $firstname = htmlentities($row['1']);
-    $lastname = htmlentities($row['2']);
-    $postcode = htmlentities($row['3']);
+    $firstname = htmlentities($row['firstname']);
+    $lastname = htmlentities($row['lastname']);
+    $postcode = htmlentities($row['postcode']);
 
     echo $firstname . ' ' . $lastname . ' ' . $postcode . '<br>';
 }
